275e3e85c74194ad31e033085c4719805d26a013
Rares Catalin Onescu <raresonescu@gmail.com>
2021-06-02 01:15:22 +0300

    se vad folderele si fisierele, fara icoana pot sa intru in foldere

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -29,55 +29,61 @@ namespace WinFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.fileNameLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.fileTypeLabel = new System.Windows.Forms.Label();
+            this.iconList = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(-6, 33);
+            this.listView1.LargeImageList = this.iconList;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(945, 268);
+            this.listView1.Size = new System.Drawing.Size(1142, 562);
+            this.listView1.SmallImageList = this.iconList;
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
             // 
-            // button1
+            // backButton
             // 
-            this.button1.Location = new System.Drawing.Point(2, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Back";
-            this.button1.UseVisualStyleBackColor = true;
+            this.backButton.Location = new System.Drawing.Point(2, 11);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // FilePathTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(83, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(774, 23);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.FilePathTextBox.Location = new System.Drawing.Point(83, 12);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.Size = new System.Drawing.Size(990, 23);
+            this.FilePathTextBox.TabIndex = 2;
+            this.FilePathTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button2
+            // goButton
             // 
-            this.button2.Location = new System.Drawing.Point(864, 11);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Go";
-            this.button2.UseVisualStyleBackColor = true;
+            this.goButton.Location = new System.Drawing.Point(1079, 12);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 3;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 417);
+            this.label1.Location = new System.Drawing.Point(40, 606);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 15);
             this.label1.TabIndex = 4;
@@ -87,7 +93,7 @@ namespace WinFormsApp1
             // fileNameLabel
             // 
             this.fileNameLabel.AutoSize = true;
-            this.fileNameLabel.Location = new System.Drawing.Point(113, 417);
+            this.fileNameLabel.Location = new System.Drawing.Point(141, 606);
             this.fileNameLabel.Name = "fileNameLabel";
             this.fileNameLabel.Size = new System.Drawing.Size(17, 15);
             this.fileNameLabel.TabIndex = 5;
@@ -96,7 +102,7 @@ namespace WinFormsApp1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(769, 417);
+            this.label3.Location = new System.Drawing.Point(964, 606);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 15);
             this.label3.TabIndex = 6;
@@ -105,24 +111,30 @@ namespace WinFormsApp1
             // fileTypeLabel
             // 
             this.fileTypeLabel.AutoSize = true;
-            this.fileTypeLabel.Location = new System.Drawing.Point(884, 417);
+            this.fileTypeLabel.Location = new System.Drawing.Point(1079, 606);
             this.fileTypeLabel.Name = "fileTypeLabel";
             this.fileTypeLabel.Size = new System.Drawing.Size(17, 15);
             this.fileTypeLabel.TabIndex = 7;
             this.fileTypeLabel.Text = "--";
             // 
+            // iconList
+            // 
+            this.iconList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.iconList.ImageSize = new System.Drawing.Size(48, 48);
+            this.iconList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 450);
+            this.ClientSize = new System.Drawing.Size(1161, 672);
             this.Controls.Add(this.fileTypeLabel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.fileNameLabel);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.FilePathTextBox);
+            this.Controls.Add(this.backButton);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -135,13 +147,14 @@ namespace WinFormsApp1
         #endregion
 
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.TextBox FilePathTextBox;
+        private System.Windows.Forms.Button goButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label fileNameLabel;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label fileTypeLabel;
+        private System.Windows.Forms.ImageList iconList;
     }
 }
 
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,12 +6,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private string filePath = "R:";
+        private bool isFile = false;
+        private string currentlySelectedItemName = "";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +23,35 @@ namespace WinFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            FilePathTextBox.Text = filePath;
+            loadFilesAndDirectories();
+        }
 
+        public void loadFilesAndDirectories()
+        {
+            DirectoryInfo fileList;
+            try
+            {
+                fileList = new DirectoryInfo(filePath);
+                FileInfo[] files = fileList.GetFiles();
+                DirectoryInfo[] dirs = fileList.GetDirectories();
+
+                listView1.Items.Clear();
+
+                for(int i = 0; i < files.Length; i++)
+                {
+                    listView1.Items.Add(files[i].Name);
+                }
+                for(int i=0; i< dirs.Length; i++)
+                {
+                    listView1.Items.Add(dirs[i].Name);
+                }
+
+            }
+            catch(Exception e)
+            {
+
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -35,6 +67,34 @@ namespace WinFormsApp1
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public void loadButtonAction()
+        {
+            filePath = FilePathTextBox.Text;
+            loadFilesAndDirectories();
+            isFile = false;
+
+        }
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            loadButtonAction();
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            currentlySelectedItemName = e.Item.Text;
+
+            FileAttributes fileAttr = File.GetAttributes(filePath + "/" + currentlySelectedItemName);
+            if((fileAttr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                isFile = false;
+                FilePathTextBox.Text = filePath + "/" + currentlySelectedItemName;
+            }
+            else
+            {
+                isFile = true;
+            }
         }
     }
 }
